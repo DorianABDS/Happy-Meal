2bd997b2d2fd77d64b99eb63dec1bb2fd52820d9
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-03-27 16:42:07 +0100

    feat(data): add ajax

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,20 @@
-first commit
+if (localStorage.getItem('recettes')) {
+    console.log('Données récupérées du localStorage :');
+    console.log(JSON.parse(localStorage.getItem('recettes')));
+  } else {
+    console.log('Chargement depuis le fichier JSON...');
+    $.ajax({
+      url: '/data/data.json',
+      method: 'GET',
+      dataType: 'json',
+      success: function (data) {
+
+        localStorage.setItem('recettes', JSON.stringify(data));
+        console.log('Données stockées dans le localStorage :', data);
+
+      },
+      error: function (xhr, status, error) {
+        console.error('Erreur lors du chargement des données :', error);
+      }
+    });
+  }
